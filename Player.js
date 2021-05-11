5c2546edf3a1adebd4179e757f86b4c3b95e50ea
Anadya Nair official✔ <71205867+AnadyaNair@users.noreply.github.com>
2021-05-11 18:00:40 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,47 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+
+  getCarsAtEnd(){
+    database.ref("carsAtEnd").on("value", (data) =>{
+      this.rank = data.val();
+    })
+  }
+  static updateCarsAtEnd(rank){
+    database.ref("/").update({
+      carsAtEnd:rank
+    })
+  }
+}
